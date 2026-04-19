1482ae8d78790b9d24013fc35c9a6eb2c94d26f7
Robert Kwiatkowski <robert.kwiatkowski01@gmail.com>
2026-04-19 14:19:20 +0200

    Initial commit

---
+++ frontend/js/api.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = "http://127.0.0.1:8000";
+
+async function getQuizzes() {
+  const response = await fetch(`${API_BASE_URL}/api/quizzes`);
+  if (!response.ok) {
+    throw new Error("Failed to load quizzes");
+  }
+  return response.json();
+}
