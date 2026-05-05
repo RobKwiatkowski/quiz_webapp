1a9d08891dc5664312093cce237aa7c88c2a9641
Robert Kwiatkowski <robert.kwiatkowski01@gmail.com>
2026-05-05 13:38:52 +0200

    fix

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "/quiz-api";
 
 async function getQuizzes() {
   const response = await fetch(`${CONFIG.API_BASE_URL}/api/quizzes`);
